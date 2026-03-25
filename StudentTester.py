703a76ac83bc58f97f26a450bf32c4551dc20367
LexGuo <lexguo3001@gmail.com>
2026-03-25 17:00:34 -0400

    Updated find_student_id() to handle validation

--- StudentTester.py
+++ StudentTester.py
@@ -97,119 +97,127 @@ def find_student_id(student_list: list[Student]):
         print(f"{student_id}: {student_list[index].get_name_last()}, {student_list[index].get_name_first()}")
 
     target_id = int(input("Enter the id of the student: "))
+    index = ids.index(target_id)
 
-    try:
-        return ids.index(target_id)
-    except ValueError:
-        print("Student ID not found.")
-        return -1
+    while index == -1:
+        target_id = int(input("ID not found. Enter the id of the student: "))
+        index = ids.index(target_id)
+
+    return index
 
 #Edit Info
 def edit_info(student_list: list[Student]):
     index = find_student_id(student_list)
-    if index == -1:
-        return
 
     student = student_list[index]
     print("This Is The Current Student Info")
     while True:
         print(
-        f"1. First Name: {student.get_name_first()}\n"
-        f"2. Middle Name: {student.get_name_last()}\n"
-        f"3. Last Name: {student.get_name_middle()}\n"
-        
-        f"4. Street Address: {student.get_mailing_address().get_street_address()}\n"
-        f"5. City Name: {student.get_mailing_address().get_city()}\n"
-        f"6. State: {student.get_mailing_address().get_state()}\n"
-        f"7. Zip Code: {student.get_mailing_address().get_zip_code()}\n"
-        f"8. Mail Type: {student.get_mailing_address().get_address_type()}\n"
-    #Email And Phone Here
-        '''
-        f"9. Email Address: {}\n"
-        f"10. Email Type: {}\n"
-        f"11. Phone Number: {}\n"
-        f"12. Phone Type: {}\n"
-        '''
-    #Rest
-        f"13. Birth Month: {student.get_birth_date().get_month()}\n"
-        f"14. Birth Day: {student.get_birth_date().get_day()}\n"
-        f"15. Birth Year: {student.get_birth_date().get_year()}\n"
-        
-        f"16. Enrollment Month: {student.get_acceptance_date().get_month()}\n"
-        f"17. Enrollment Day: {student.get_acceptance_date().get_day()}\n"
-        f"18. Enrollment Year: {student.get_acceptance_date().get_year()}\n"
-        
-        f"19. Starting Semester Month: {student.get_semester_start().get_month()}\n"
-        f"20. Starting Semester Day: {student.get_semester_start().get_day()}\n"
-        f"21. Starting Semester Year: {student.get_semester_start().get_year()}\n"
-        f"22. Major: {student.get_major()}\n"
+            # Names
+            f"1. First Name: {student.get_name_first()}\n"
+            f"2. Middle Name: {student.get_name_last()}\n"
+            f"3. Last Name: {student.get_name_middle()}\n"
+            
+            # Address
+            f"4. Street Address: {student.get_mailing_address().get_street_address()}\n"
+            f"5. City Name: {student.get_mailing_address().get_city()}\n"
+            f"6. State: {student.get_mailing_address().get_state()}\n"
+            f"7. Zip Code: {student.get_mailing_address().get_zip_code()}\n"
+            f"8. Mail Type: {student.get_mailing_address().get_address_type()}\n"
+            
+            # Email And Phone
+            '''
+            f"9. Email Address: {}\n"
+            f"10. Email Type: {}\n"
+            f"11. Phone Number: {}\n"
+            f"12. Phone Type: {}\n"
+            '''
+            
+            # Misc
+            f"13. Birth Month: {student.get_birth_date().get_month()}\n"
+            f"14. Birth Day: {student.get_birth_date().get_day()}\n"
+            f"15. Birth Year: {student.get_birth_date().get_year()}\n"
+            
+            f"16. Enrollment Month: {student.get_acceptance_date().get_month()}\n"
+            f"17. Enrollment Day: {student.get_acceptance_date().get_day()}\n"
+            f"18. Enrollment Year: {student.get_acceptance_date().get_year()}\n"
+            
+            f"19. Starting Semester Month: {student.get_semester_start().get_month()}\n"
+            f"20. Starting Semester Day: {student.get_semester_start().get_day()}\n"
+            f"21. Starting Semester Year: {student.get_semester_start().get_year()}\n"
+            f"22. Major: {student.get_major()}\n"
         )
-        print()
-        User_Choice = int(input("Please Enter The Number of the info you'd like to change:"))
+        user_choice = int(input("Please Enter The Number of the info you'd like to change:"))
+
         #Names
-        if User_Choice == 1:
+        if user_choice == 1:
             new_input1 = input("Please Enter A New First Name:")
             student.set_name_first(new_input1)
-        elif User_Choice == 2:
+        elif user_choice == 2:
             new_input2 = input("Please Enter A New Last Name:")
             student.set_name_last(new_input2)
-        elif User_Choice == 3:
+        elif user_choice == 3:
             new_input3 = input("Please Enter A New Middle Name:")
             student.set_name_middle(new_input3)
 
-        #Mailing Address
-        elif User_Choice == 4:
+        # Mailing Address
+        elif user_choice == 4:
             new_input4 = input("Please Enter A New Street Address:")
             student.get_mailing_address().set_street_address(new_input4)
-        elif User_Choice == 5:
+        elif user_choice == 5:
             new_input5 = input("Please Enter A New City:")
             student.get_mailing_address().set_city(new_input5)
-        elif User_Choice == 6:
+        elif user_choice == 6:
             new_input6 = input("Please Enter A New State:")
             student.get_mailing_address().set_state(new_input6)
-        elif User_Choice == 7:
+        elif user_choice == 7:
             new_input7 = input("Please Enter A New Zip Code:")
             student.get_mailing_address().set_zip_code(new_input7)
-        elif User_Choice == 8:
+        elif user_choice == 8:
             new_input8 = input("Please Enter A New Address Type:")
             student.get_mailing_address().set_address_type(new_input8)
-            '''
-            #Choices corresponding with email and phone 
-        elif User_Choice == 9:
-        elif User_Choice == 10:
-        elif User_Choice == 11:
-        elif User_Choice == 12:
-        '''
-        elif User_Choice == 13:
+
+        # Email and Phone
+        elif user_choice == 9:
+            pass
+        elif user_choice == 10:
+            pass
+        elif user_choice == 11:
+            pass
+        elif user_choice == 12:
+            pass
+
+        # Misc
+        elif user_choice == 13:
             new_input13 = input("Please Enter A New Birth Month:")
             student.get_birth_date().set_month(new_input13)
-        elif User_Choice == 14:
+        elif user_choice == 14:
             new_input14 = input("Please Enter A New Birth Day:")
             student.get_birth_date().set_day(new_input14)
-        elif User_Choice == 15:
+        elif user_choice == 15:
             new_input15 = input("Please Enter A New Birth Year:")
             student.get_birth_date().set_year(new_input15)
 
-        elif User_Choice == 16:
+        elif user_choice == 16:
             new_input16 = input("Please Enter A New Enrollment Month:")
             student.get_acceptance_date().set_month(new_input16)
-        elif User_Choice == 17:
+        elif user_choice == 17:
             new_input17 = input("Please Enter A New Enrollment Day:")
             student.get_acceptance_date().set_day(new_input17)
-        elif User_Choice == 18:
+        elif user_choice == 18:
             new_input18 = input("Please Enter A New Enrollment Year:")
             student.get_acceptance_date().set_year(new_input18)
 
-        elif User_Choice == 19:
+        elif user_choice == 19:
             new_input19 = input("Please Enter A New Starting Semester Month:")
             student.get_semester_start().set_month(new_input19)
-        elif User_Choice == 20:
+        elif user_choice == 20:
             new_input20 = input("Please Enter A New Starting Semester Day:")
             student.get_semester_start().set_month(new_input20)
-        elif User_Choice == 21:
+        elif user_choice == 21:
             new_input21 = input("Please Enter A New Starting Semester Year:")
             student.get_semester_start().set_month(new_input21)
-        elif User_Choice == 22:
+        elif user_choice == 22:
             new_input22 = input("Please Enter A New Major:")
             student.set_major(new_input22)
 
@@ -232,23 +240,18 @@ def main():
             student_list.append(create_student())
 
         elif user_input == 2: # Edit student
-            edit = edit_info(student_list)
+            edit_info(student_list)
 
         elif user_input == 3: # Remove student
             student_index = find_student_id(student_list)
-            if student_index == -1:
-                print("Student ID not found")
-            else:
-                confirmation = input("Are you sure you wish to delete? (Type Yes/No):")
-                if confirmation == "Yes" or "yes":
-                    student_list.remove(student_list[student_index])
-                    print("Student Successfully Removed")
+            confirmation = input("Are you sure you wish to delete? (Type Yes/No):").lower()
+            if confirmation == "yes":
+                student_list.remove(student_list[student_index])
+                print("Student Successfully Removed")
 
         elif user_input == 4: # Display student
             student_index = find_student_id(student_list)
-
-            if student_index != -1:
-                print(student_list[student_index])
+            print(student_list[student_index])
 
         #Loop Breaks here
         elif user_input == 5:
